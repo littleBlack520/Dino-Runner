276dfb8c42c3e7760b1c298ea3b1bf86e816593f
guing <aguing@outlook.com>
2018-04-17 22:31:35 +0800

    添加小恐龙

---
+++ js/player.js
@@ -0,0 +1,22 @@
+let canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+const HEIGHT = canvas.height;
+const WIDTH = canvas.width;
+function draw() {
+    var img = new Image();
+    img.onload = function () {
+        var x = 1678,
+            index = 2;
+        setInterval(function(){
+            if(index > 3){
+                index = 2;
+            }
+            x = 1678 + (88*index)
+            ctx.clearRect(0, 0, WIDTH, HEIGHT);
+            ctx.drawImage(img, x,2,88,95,0,HEIGHT-43,40,43);
+            index++;
+        },100);
+    }
+    img.src = '../images/pic.png';
+}
+draw();
