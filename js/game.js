18c1c262e43bfd44e9f6aa408f9807746758bb1a
guing <aguing@outlook.com>
2018-04-18 01:03:39 +0800

    添加玩家类

--- js/game.js
+++ js/game.js
@@ -1,43 +1,57 @@
 class Game {
   constructor(id) {
     let canvas = document.getElementById(id);
-    this.content = canvas.getContext("2d");
+    this.context = canvas.getContext("2d");
     this.canvas = canvas;
+    this.buildGame();
   }
   /**
    * 加载资源
    */
-  loadAssets(){}
+  loadAssets() {
+    const PATH = "../images/pic.png";
+    let sprite  = new Image();
+    sprite.src = PATH;
+    return new Promise((resolve, reject)=>{
+      sprite.onload = ()=> {
+        resolve(sprite);
+      };
+    });
+  }
   /**
    * 绘制UI
    */
-  drawUI(){}
+  drawUI() {}
   /**
    * 绘制背景
    */
-  drawBG(){}
+  drawBG() {}
   /**
    * 绘制玩家
    */
-  drawPlayer(){}
+  drawPlayer() {}
   /**
    * 绘制敌人
    */
-  drawEnemy(){}
+  drawEnemy() {}
   /**
    * 设置操作
    */
-  setControll(){}
+  setControll() {}
   /**
    * 构建游戏
    */
-  buildGame(){}
+  buildGame() {
+    this.loadAssets().then(sprite=>{
+        new Player(this.context,0,0,sprite);
+    });
+  }
   /**
    * 更新游戏
    */
-  updateGame(){}
+  updateGame() {}
   /**
    * 结束游戏
    */
-  overGame(){}
+  overGame() {}
 }
--- js/player.js
+++ js/player.js
@@ -1,18 +1,69 @@
-
-function draw() {
-    var img = new Image();
-    img.onload = function () {
-        var x = 1678,
-            index = 2;
-        setInterval(function(){
-            if(index > 3){
-                index = 2;
-            }
-            x = 1678 + (88*index)
-            ctx.clearRect(0, 0, WIDTH, HEIGHT);
-            ctx.drawImage(img, x,2,88,95,0,HEIGHT-43,40,43);
-            index++;
-        },100);
-    }
-    img.src = '../images/pic.png';
+/**
+ * 玩家类
+ */
+class Player {
+  /**
+   * 构造函数
+   * @param {绘制对象} context 
+   * @param {x位置} x 
+   * @param {y位置} y 
+   * @param {源图像} sprite 
+   */
+  constructor(context, x, y, sprite) {
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.sprite = sprite;
+    //不同状态的位置点
+    this.spritePos = {
+      body: { x: 76, y: 6 },
+      run: [{ x: 1854, y: 2 }, { x: 1942, y: 2 }],
+      die: { x: 2030, y: 2 }
+    };
+    this.width = 88; //宽度
+    this.height = 95; //高度
+    this.scale = 0.5; //源图像的缩放比例
+    this.speed = 200; //速度
+    this.create(this.spritePos.body.x, this.spritePos.body.y);
+  }
+  /**
+   * 创建
+   * @param {x位置点} sx 
+   * @param {y位置点} sy 
+   */
+  create(sx, sy) {
+    let source = this.sprite,
+      sw = this.width,
+      sh = this.height,
+      x = this.x,
+      y = this.y,
+      w = sw * this.scale,
+      h = sh * this.scale;
+    this.context.clearRect(x, y, w, h);
+    this.context.drawImage(source, sx, sy, sw, sh, x, y, w, h);
+  }
+  /**
+   * 跑
+   */
+  run() {
+    let index = 0;
+    let func = () => {
+      setTimeout(() => {
+        let pos =  this.spritePos.run[index],
+            sx = pos.x,
+            sy = pos.y;
+        this.create(sx,sy);
+        index++;
+        index =  index>=this.spritePos.run.length?0:index;
+        func();
+      }, this.speed);
+    };
+    func();
+  }
+  /**
+   * 死
+   */
+  die(){
+    this.create(this.spritePos.die.x, this.spritePos.die.y);
+  }
 }
